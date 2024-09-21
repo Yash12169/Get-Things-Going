aa07987ac73dcfb9a0c9c4f5469800a9a45e0dfd
yash <jewalkarayash@gmail.com>
2024-09-21 13:32:12 +0530

    add list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { VanishList } from './components/hero/Hero';
+
+function App() {
+  return (
+   <>
+    <VanishList/>
+   </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
